ec633a170f04edf31152da523801e91444364f8e
Vadim Li <nog453010@gmail.com>
2026-01-17 17:46:59 +0500

    First commit

---
+++ src/init.py
@@ -0,0 +1,6 @@
+"""telescope package marker.
+
+This file exists to satisfy the required repo layout. The runtime entry point
+is app.py, which can be invoked directly. We avoid adding extra files to keep
+structure minimal and explicit for an MVP.
+"""
